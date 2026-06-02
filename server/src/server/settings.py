fd0a4be2b2ece773b746a220bb56b4a4ea0a5f87
Keane te Velde <keanetevelde@gmail.com>
2026-06-02 16:11:38 +0200

    Added option to switch to sqlite for local testing

--- server/src/server/settings.py
+++ server/src/server/settings.py
@@ -73,18 +73,24 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-
+USE_SQLITE = config('USE_SQLITE', default=False, cast=bool)
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'psedb',
-        'USER': config('POSTGRESQL_USER'),
-        'PASSWORD': config('POSTGRESQL_PASSWORD'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+if USE_SQLITE:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 
 # Password validation
