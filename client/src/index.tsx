64bf453a6854b732a92d9f873c971f15200bb762
ZJWeng <weng.zj.2005@gmail.com>
2026-06-02 22:24:17 +0200

    basic water template

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,23 @@
 import Toolbar from '@/components/widgets/toolbar';
 import { PortalHost } from '@rn-primitives/portal';
+import { useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import '../global.css';
 
 export default function App() {
+  const [water, setWater] = useState(10)
+
+  function click() {
+    if (water < 100) {
+      setWater(water+10);
+    }
+  }
+
+  function resetWater() {
+    setWater(0);
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView className='bg-yellow-200 h-full'>
