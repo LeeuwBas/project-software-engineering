48a22bbd569d9aba8c638a98ea10442e11a87ee6
WilliamBower <william.mat.bower@gmail.com>
2026-06-03 13:04:22 +0200

    Quotes to text instead of database.

--- server/src/server/tests.py
+++ server/src/server/tests.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 
-# TODO Make new tests
 class RandomQuoteAPITests(APITestCase):
     def setUp(self):
         self.url = reverse('random-quote')
@@ -17,7 +16,6 @@ class RandomQuoteAPITests(APITestCase):
         self.assertEqual(response.data['error'], 'No quotes found in the database.')
 
     def test_get_random_quote_success(self):
-<<<<<<< HEAD
         try:
             with open('resources/quotes.txt', 'r') as f:
                 lines = f.readlines()
@@ -32,13 +30,3 @@ class RandomQuoteAPITests(APITestCase):
             self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
             self.assertIn('error', response.data)
             self.assertEqual(response.data['error'], 'No quotes found in the database.')
-=======
-        quote1 = Quote.objects.create(text='To be or not to be.')
-        quote2 = Quote.objects.create(text='I think, therefore I am.')
-
-        response = self.client.get(self.url)
-
-        # Expect a code 200 with one of the 2 quotes.
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn(response.data['text'], [quote1.text, quote2.text])
->>>>>>> b9087bf (single quote style)
