6ec6d43d6736ac60dbdff4d1a2b49c18fc9407c2
ZJWeng <weng.zj.2005@gmail.com>
2026-06-03 13:34:26 +0200

    removed testing page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,50 +1,16 @@
 import { PortalHost } from '@rn-primitives/portal';
 import { Text } from 'react-native';
-import { Button } from '@/components/ui/button';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { useState } from 'react';
 
 import '../global.css';
 
 export default function App() {
-  const [water, setWater] = useState(10)
-
-  function click() {
-    if (water < 100) {
-      setWater(water+10);
-    }
-  }
-
-  function resetWater() {
-    setWater(0);
-  }
-
   return (
     <SafeAreaProvider>
       <SafeAreaView className={styles.container}>
-        <Button variant="outline" onPress={click}>
-          <Text className={styles.text}>
-            Drink
-          </Text>
-        </Button>
-        <Text>
-          {water}
-        </Text>
-        <Button variant='destructive' onPress={resetWater}>
-          <Text>
-            Reset
-          </Text>
-        </Button>
+        <Text>Hello World!</Text>
       </SafeAreaView>
     <PortalHost />
     </SafeAreaProvider>
   );
-}
-
-
-
-const styles = {
-  container: 'items-center justify-center',
-  button: 'bg-black',
-  text: 'text-blue-500 font-bold',
 }
