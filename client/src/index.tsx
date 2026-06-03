38962bd9ef571d7a5a811b58970d52e3becbb314
ZJWeng <weng.zj.2005@gmail.com>
2026-06-03 13:34:26 +0200

    removed testing page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,23 +1,10 @@
 import Toolbar from '@/components/widgets/toolbar';
 import { PortalHost } from '@rn-primitives/portal';
-import { useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import '../global.css';
 
 export default function App() {
-  const [water, setWater] = useState(10)
-
-  function click() {
-    if (water < 100) {
-      setWater(water+10);
-    }
-  }
-
-  function resetWater() {
-    setWater(0);
-  }
-
   return (
     <SafeAreaProvider>
       <SafeAreaView className='bg-yellow-200 h-full'>
@@ -28,4 +15,3 @@ export default function App() {
     </SafeAreaProvider>
   );
 }
-
