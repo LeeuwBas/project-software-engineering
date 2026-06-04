ceb0b3999e0d629e1c3960c0e570fe6ce48bddd7
ZJWeng <weng.zj.2005@gmail.com>
2026-06-04 11:47:59 +0200

    added responsiveness to selected stat

--- client/src/components/widgets/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native'
-
-export default function Sidebar({value} : {value : number}) {
-    return (
-        <View className='my-auto border-2 border-gray-500 w-full h-3/4 rounded-xl'>
-            <View className='mt-auto bg-blue-400 rounded-xl w-full' style={{height: `${value}%`}} />
-        </View>
-    )
-}
