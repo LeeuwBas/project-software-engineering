82f010949b0fb96eea7892c362b99562eafcda5b
ZJWeng <weng.zj.2005@gmail.com>
2026-06-04 21:40:26 +0200

    added onigiri sprite

--- client/src/components/widgets/Tamagotchi.tsx
+++ client/src/components/widgets/Tamagotchi.tsx
@@ -4,7 +4,7 @@ import { View } from "react-native";
 export default function Tamagotchi() {
     return (
         <View className="w-3/5 mx-auto">
-            <Image style={{width: "100%", aspectRatio: 1}} contentFit="cover" source={require("@/../assets/placeholders/placeholder_tama.webp")}/>
+            <Image style={{width: "100%", aspectRatio: 1}} contentFit="cover" source={require("@/../assets/onigiri_sprite.webp")}/>
         </View>
     );
 }
