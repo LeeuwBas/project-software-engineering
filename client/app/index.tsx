5b1c41d30d6f04e952d58bcb1ee6c9dd30dd0c75
ZJWeng <weng.zj.2005@gmail.com>
2026-06-04 11:02:38 +0200

    moved main homepage (tamagotchi and sidebars) to separate file

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -5,10 +5,6 @@ import Topbar from '@/components/widgets/topbar';
 import * as storage from '@/lib/storage';
 import { BlurView } from 'expo-blur';
 import { useState } from 'react';
-<<<<<<< HEAD
-=======
-import { View } from 'react-native';
->>>>>>> d93e88a (moved topbar to separate file)
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import '../global.css';
