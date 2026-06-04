af3e6cbc1fe509c5c3c90f494c8280f3984534fd
david kramer <david.kramer2@student.uva.nl>
2026-06-04 11:47:01 +0200

    fixed the broken merge

--- server/src/server/urls.py
+++ server/src/server/urls.py
@@ -45,4 +45,9 @@ urlpatterns = [
     ),
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/stats/admin_insert', StatInsertView.as_view(), name='stat_insert_view'),
+    path('api/stats/metrics', StatsWaterRequestAverage.as_view(), name='water_metrics'),
+    path('api/stats/addwater', StatsWaterUpdate.as_view(), name='update_water'),
+    path('api/stats/waterchart', StatsWaterBarChart.as_view(), name='water_chart'),
 ]
