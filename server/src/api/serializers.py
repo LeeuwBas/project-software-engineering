b69b620e31532f65a0db20dc52186cec129ffc66
WilliamBower <william.mat.bower@gmail.com>
2026-06-05 14:50:24 +0200

    Fixed merge comments

--- server/src/api/serializers.py
+++ server/src/api/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import User, Stats
 
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, min_length=8)
 
@@ -13,7 +14,17 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
 
+
 class StatsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stats
         fields = "__all__"
+
+
+class QuoteRequestSerializer(serializers.Serializer):
+    mood = serializers.CharField(max_length=100)
+    action = serializers.CharField(max_length=100)
+
+
+class QuoteResponseSerializer(serializers.Serializer):
+    quote = serializers.CharField()
--- server/src/server/urls.py
+++ server/src/server/urls.py
@@ -32,29 +32,29 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r"users", UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/get-quote/', RequestQuote.as_view(), name='RequestQuote'),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/stats/admin_insert', StatInsertView.as_view(), name='stat_insert_view'),
-    path('api/stats/metrics', StatsWaterRequestAverage.as_view(), name='water_metrics'),
-    path('api/stats/addwater', StatsWaterUpdate.as_view(), name='update_water'),
-    path('api/stats/waterchart', StatsWaterBarChart.as_view(), name='water_chart'),
-    path('api/stats/view', StatisticsView.as_view(), name='statview'),
+    path("api/get-quote/", RequestQuote.as_view(), name="RequestQuote"),
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/stats/admin_insert", StatInsertView.as_view(), name="stat_insert_view"),
+    path("api/stats/metrics", StatsWaterRequestAverage.as_view(), name="water_metrics"),
+    path("api/stats/addwater", StatsWaterUpdate.as_view(), name="update_water"),
+    path("api/stats/waterchart", StatsWaterBarChart.as_view(), name="water_chart"),
+    path("api/stats/view", StatisticsView.as_view(), name="statview"),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
-        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
         path(
-            'api/schema/swagger-ui/',
-            SpectacularSwaggerView.as_view(url_name='schema'),
-            name='swagger-ui',
+            "api/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
         ),
     ]
