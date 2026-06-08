d9e523537a5770d05cb826b99e81de08e773d184
david kramer <david.kramer2@student.uva.nl>
2026-06-08 14:14:54 +0200

    fixed imports

--- server/src/server/urls.py
+++ server/src/server/urls.py
@@ -21,13 +21,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from api.views import UserViewSet, StatInsertView
-from server.src.api.statistics.views import (
+from api.statistics.views import (
     StatsWaterRequestAverage,
     StatsWaterUpdate,
     StatsWaterBarChart,
     StatisticsView,
 )
-from server.src.api.quotes.views import RequestQuote
+from api.quotes.views import RequestQuote
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
