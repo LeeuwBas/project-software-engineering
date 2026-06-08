68451829dc5ac42af06efa82ff2d9b8220386427
ZJWeng <weng.zj.2005@gmail.com>
2026-06-08 17:10:42 +0200

    Topbar redesign

--- client/src/components/widgets/topbar.tsx
+++ client/src/components/widgets/topbar.tsx
@@ -1,21 +1,48 @@
-import { Heart } from 'lucide-react-native';
-import { Text, View } from 'react-native';
+import { Sun } from 'lucide-react-native';
+import { View } from 'react-native';
 import { AppText } from '../AppText';
+
+function Mood(happiness: number) {
+    switch (true) {
+        case happiness < 20:
+            return 'Sad'
+        case happiness < 40:
+            return 'Gloomy'
+        case happiness < 60:
+            return 'Neutral'
+        case happiness < 80:
+            return 'Content'
+    }
+    return 'Happy'
+}
 
 export default function Topbar() {
     const date = new Date();
     const day = date.toLocaleDateString('en-US', { weekday: 'short' })
 
+    const happiness = 30
+    const mood = Mood(happiness)
+
     return (
         <View className="flex flex-row items-center">
-            <View className='ml-5'>
-                <AppText className=' text-2xl font-bold'>{day}</AppText>
+            <View className='ml-5 w-1/2 flex-col gap-2'>
+                {/* TODO: Add logic with weather API */}
+                <View className='flex-row'>
+                    <Sun size={30}/>
+                    <AppText className=' text-2xl font-bold'>{day}</AppText>
+                </View>
             </View>
 
-            <View className='ml-auto flex-row gap-2 mr-10'>
-                {Array.from({ length: 3 }).map((_, index) => (
-                <Heart key={index} fill={"#FF0000"} />
-                ))}
+            <View className='mx-auto w-1/3'>
+                <AppText className='mx-auto'>Mood: {mood}</AppText>
+                <View className='h-5 border-4 border-gray-500 bg-white'>
+                    <View
+                        className='mr-auto bg-[#ffd568] h-full'
+                        style={{
+                            width: `${happiness}%`,
+                        }}
+                        />
+                </View>
             </View>
         </View>
     )
