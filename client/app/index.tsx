2ba25b751df90669e1264f9e8ab96fb26781337a
buenk <ezra.buenk@anove.agency>
2026-06-09 19:01:50 +0200

    Add h-full to home page view

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -13,11 +13,11 @@ import '../global.css';
 export default function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [settingsOpen, setSettingsOpen] = useState(false);
-  const {water, saveWater} = storage.useWater(menuOpen);
+  const { water, saveWater } = storage.useWater(menuOpen);
   const appState = useRef(AppState.currentState);
 
   useEffect(() => {
-    const subscription = AppState.addEventListener('change', nextAppState => {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
       if (nextAppState === 'background') {
         triggerBackup();
       }
@@ -35,45 +35,51 @@ export default function App() {
   };
 
   function changeMenu() {
-    setMenuOpen(!menuOpen)
-  };
-  
+    setMenuOpen(!menuOpen);
+  }
+
   function changeSettings() {
-    setSettingsOpen(!settingsOpen)
-  };
+    setSettingsOpen(!settingsOpen);
+  }
 
   function closePopup() {
-    if (popup.menuOpen) setMenuOpen(false)
-    if (popup.settingsOpen) setSettingsOpen(false)
+    if (popup.menuOpen) setMenuOpen(false);
+    if (popup.settingsOpen) setSettingsOpen(false);
   }
 
   const popup: PopupConfigs = {
     menuOpen: menuOpen,
     changeMenu: changeMenu,
     settingsOpen: settingsOpen,
-    changeSettings: changeSettings
-  }
+    changeSettings: changeSettings,
+  };
 
-  const popupOpen = menuOpen || settingsOpen
+  const popupOpen = menuOpen || settingsOpen;
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView className='bg-[#74c6b6]/85'>
-        <View className='flex flex-col size-full bg-[#74c6b6]/30'>
-          <Pressable className='absolute inset-0 size-full z-10' onPress={closePopup}/>
-          
+      <SafeAreaView className="h-full bg-[#74c6b6]/85">
+        <View className="flex size-full flex-col bg-[#74c6b6]/30">
+          <Pressable className="absolute inset-0 z-10 size-full" onPress={closePopup} />
+
           <Topbar />
 
-          <Main leftSideStat='water' leftSideValue={water} rightSideStat='none' rightSideValue={0}/>
+          <Main
+            leftSideStat="water"
+            leftSideValue={water}
+            rightSideStat="none"
+            rightSideValue={0}
+          />
 
           {/* The blur that appears when popup menu is opened */}
-          <BlurView className={`absolute w-full h-full transition-opacity duration-300 ${ popupOpen ? 'opacity-100' : 'opacity-0' }`} intensity={40} tint='regular' experimentalBlurMethod='dimezisBlurView'/>
+          <BlurView
+            className={`absolute h-full w-full transition-opacity duration-300 ${popupOpen ? 'opacity-100' : 'opacity-0'}`}
+            intensity={40}
+            tint="regular"
+            experimentalBlurMethod="dimezisBlurView"
+          />
 
-          <Toolbar
-            popup={popup}
-            water={water}
-            saveWater={saveWater}
-            />
+          <Toolbar popup={popup} water={water} saveWater={saveWater} />
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
