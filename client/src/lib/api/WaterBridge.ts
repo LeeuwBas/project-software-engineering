53e35e2961835957517223c4a25485cc82f83176
LeeuwBas <bas.leeuw@student.uva.nl>
2026-06-09 18:17:54 +0200

    Add a more complex simple API?

--- client/src/lib/api/WaterBridge.ts
+++ client/src/lib/api/WaterBridge.ts
@@ -1,23 +1,62 @@
-import {useWater} from "@/lib/storage";
+import {create} from "zustand";
+import {LoadableBridge} from "@/lib/api/APIBridge";
 
+// Use the water bridge when the values need to be manipulated.
 export interface WaterBridge {
-    getWater: () => Promise<number>,
     addWater: (value: number) => Promise<number>,
 }
 
-export function createWaterBridge(): WaterBridge {
+type WaterState = {
+  water: number|undefined;
+  setWater: (value: number) => void;
+};
+
+const waterState = create<WaterState>((set) => ({
+  water: undefined,
+  setWater: (value) => set({ water: value }),
+}));
+
+/**
+ * Can be used to get and subscribe to water value changes in the UI.
+ *
+ * Example:
+ * ```tsx
+ * const water = useWater() ?? 0
+ * return <AppText>{water}</AppText>
+ * ```
+ *
+ * This will update the water value whenever it is changed internally.
+ */
+export function useWater() {
+    return waterState(((s) => s.water));
+}
+
+export function createWaterBridge(): LoadableBridge<WaterBridge> {
     return {
-        getWater,
+        load,
         addWater,
     };
 }
 
-async function getWater() {
-    return useWater(false).water;
+async function load() {
+    if (waterState.getState().water !== undefined) {
+        throw Error("Water already loaded")
+    }
+
+    // TODO: load from new storage impl and API endpoints
+    const loadedWater = Math.round(Math.random() * 100)
+    waterState.getState().setWater(loadedWater);
+    return loadedWater;
 }
 
 async function addWater(value: number) {
-    const new_water = Math.max(Math.min(await getWater() + value, 100), 0);
-    useWater(false).setWater(new_water);
+    const current_water = waterState.getState().water;
+
+    if (current_water === undefined) {
+        throw Error("Water not loaded yet")
+    }
+
+    const new_water = Math.max(Math.min(current_water + value, 100), 0);
+    waterState.getState().setWater(new_water)
     return new_water;
 }
