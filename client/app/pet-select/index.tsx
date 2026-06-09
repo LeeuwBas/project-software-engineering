e56af8569d7735bf8cc2db2963938142c1a069d1
ZJWeng <weng.zj.2005@gmail.com>
2026-06-09 16:30:42 +0200

    prevent id underflow and overflow + confirm (no functionality)

--- client/app/pet-select/index.tsx
+++ client/app/pet-select/index.tsx
@@ -1,23 +1,40 @@
 import { AppText } from '@/components/AppText';
 import Pet from '@/components/widgets/Pet';
+import { useRouter } from 'expo-router';
 import { useState } from 'react';
 import { Pressable, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+const NUM_PETS = 3
+
 export default function PetSelection() {
+    const router = useRouter()
     const [id, setId] = useState(2)
+
+    function alterId(value: number) {
+        const new_id = id+value
+        if (new_id < 0 || new_id >= NUM_PETS) return
+        setId(id+value)
+    }
 
     return (
         <SafeAreaProvider>
             <SafeAreaView className='size-full justify-center'>
-                <View className='flex-row'>
-                    <Pressable className='w-1/5 bg-slate-300' onPress={() => setId(id-1)}>
-                        <AppText>Left</AppText>
-                    </Pressable>
-                    <Pet id={id}/>
-                    <Pressable className='w-1/5 bg-slate-300' onPress={() => setId(id+1)}>
-                        <AppText>Right</AppText>
-                    </Pressable>
+                <View className='flex-col gap-12'>
+                    <View className='flex-row'>
+                        <Pressable className='w-1/5 bg-slate-300' onPress={() => alterId(-1)}>
+                            <AppText>Left</AppText>
+                        </Pressable>
+                        <Pet id={id}/>
+                        <Pressable className='w-1/5 bg-slate-300' onPress={() => alterId(+1)}>
+                            <AppText>Right</AppText>
+                        </Pressable>
+                    </View>
+                    <View className='items-center'>
+                        <Pressable className='w-1/2 bg-slate-400' onPress={() => router.back()}>
+                            <AppText>Confirm</AppText>
+                        </Pressable>
+                    </View>
                 </View>
             </SafeAreaView>
         </SafeAreaProvider>
