80ad3b69be2f060d64cb14094137bd8b6e63f58c
buenk <ezra.buenk@anove.agency>
2026-06-09 19:07:45 +0200

    Fix popup card background

--- client/src/components/widgets/Menu.tsx
+++ client/src/components/widgets/Menu.tsx
@@ -19,9 +19,9 @@ export default function Menu (
 
     if (isOpen) {
         return (
-            <View className={`-top-6 transition-opacity duration-200 ${ isOpen ? 'opacity-100' : 'opacity-0' } items-center`} >
+            <View className={`-top-6 w-full transition-opacity duration-200 ${ isOpen ? 'opacity-100' : 'opacity-0' } items-center`} >
                 <View className='absolute bottom-full mb-2 items-center w-full'>
-                    <View className="w-3/4 h-auto bg-grey justify-center items-center bg-white rounded-3xl shadow-sm">
+                    <View className="w-3/4 h-auto justify-center items-center bg-white rounded-3xl shadow-sm">
                         <AppText className='m-5 font-bold text-2xl'>{name}</AppText>
                         <WaterWidget water={water} setWater={setWater}/>
                         <StepsWidget/>
