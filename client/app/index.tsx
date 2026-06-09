c3880f10bd671dbdabb2921b0958b90f1067c1f1
ZJWeng <weng.zj.2005@gmail.com>
2026-06-09 23:39:41 +0200

    replaced logic with RequireAuth

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,9 +1,8 @@
-import { useAuth } from '@/auth/AuthManager';
+import { requireAuth } from '@/auth/AuthManager';
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  const auth = useAuth();
-  if (process.env.EXPO_PUBLIC_DISABLE_AUTH === 'True') return <Redirect href="/(protected)" />;
-  if (auth?.isLoading) return null;
-  return <Redirect href={auth?.accessToken ? '/(protected)' : '/login'} />;
+  return requireAuth(
+    <Redirect href="/(protected)"/>
+  )
 }
