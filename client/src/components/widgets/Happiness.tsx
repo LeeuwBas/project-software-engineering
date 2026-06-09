e98077d59912e9aac252b44a8d33b9a0716fc38a
ZJWeng <weng.zj.2005@gmail.com>
2026-06-09 12:16:32 +0200

    added smile and frown next to happiness bar

--- client/src/components/widgets/Happiness.tsx
+++ client/src/components/widgets/Happiness.tsx
@@ -1,6 +1,7 @@
 import { AppText } from "@/components/AppText"
 import getMood from "@/lib/mood"
 import { LinearGradient } from "expo-linear-gradient"
+import { Frown, Smile } from "lucide-react-native"
 import { View } from "react-native"
 
 export default function Happiness() {
@@ -9,19 +10,23 @@ export default function Happiness() {
     const mood = getMood(happiness)
 
     return (
-        <View className='mx-auto w-1/3'>
+        <View className='w-1/2 px-2'>
             <AppText className='mx-auto'>Mood: {mood}</AppText>
-            <View className='h-5 border-4 border-gray-500 bg-white'>
-                <LinearGradient
-                    colors={['#397cf7', '#f5ec9a', '#f7df05']}
-                    start={[0, 1]}
-                    end={[1, 0]}
-                    className='mr-auto h-full w-full'
-                    />
-                <View
-                    className='absolute right-0 top-0 bottom-0 bg-white'
-                    style={{width: `${100-happiness}%`}}
-                    />
+            <View className='flex-row gap-1 justify-center'>
+                <Frown size={20}/>
+                <View className='h-5 flex-1 border-4 border-gray-500 bg-white'>
+                    <LinearGradient
+                        colors={['#397cf7', '#f5ec9a', '#f7df05']}
+                        start={[0, 1]}
+                        end={[1, 0]}
+                        className='mr-auto h-full w-full'
+                        />
+                    <View
+                        className='absolute right-0 top-0 bottom-0 bg-white'
+                        style={{width: `${100-happiness}%`}}
+                        />
+                </View>
+                <Smile size={20}/>
             </View>
         </View>
     )
