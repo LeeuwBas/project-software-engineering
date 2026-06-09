a1e8ca1df4dc887bbb59deacc3dd3354452c5cce
LeeuwBas <bas.leeuw@student.uva.nl>
2026-06-09 11:13:13 +0200

    fix test workflow?

--- server/src/server/settings.py
+++ server/src/server/settings.py
@@ -40,7 +40,6 @@ AUTH_USER_MODEL = 'authentication.User'
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
