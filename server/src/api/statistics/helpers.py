033bb98f7a827890b217764bb3797db159a213c1
david kramer <david.kramer2@student.uva.nl>
2026-06-11 21:31:15 +0200

    created endpoints to set and fetch goals

--- server/src/api/statistics/helpers.py
+++ server/src/api/statistics/helpers.py
@@ -1,10 +1,20 @@
 from django.utils import timezone
 from django.db.models import Sum, Max, Min, Avg, F
 
-from datetime import timedelta
+from datetime import timedelta, date, datetime
 
-from .models import Stats
+from .models import Stats, Goals
 
+
+def getStatDict(line: Stats | Goals):
+    """
+        Pulls all items in stats and goals models into a nice dictionary to
+        read from and update.
+
+    """
+    return {
+        'water': line.water
+    }
 
 def getSummary(days: int, user: str, statistic: str):
     filter_dict = {"user": user}
@@ -51,8 +61,52 @@ def getToday(user: str, statistic: str):
 
     filter = {
         'user': user,
-        'date': timezone.now()
+        'date': timezone.now().date()
     }
     amount = getattr(Stats.objects.filter(**filter).first(), statistic)
 
     return amount
+
+def getGoal(user: str, statName: str | None = None, day: datetime | None = None):
+    """
+        Retrieves the latest goal, either for a given stat or just for all stats
+        combined.
+
+        If no day was given, will retrieve the most recent goal.
+    """
+    if day is None:
+        day = timezone.now()
+
+    filter = {
+        'user': user,
+        'date__lte': day.date()
+    }
+
+    line = Goals.objects.filter(filter).order_by('-date').first()
+
+    if line is None:
+        line = Goals()
+
+    if statName is None:
+        return getStatDict(line)
+
+    return getattr(line, statName, None)
+
+def setGoal(user: str, goal_data: dict[str, int], day: datetime | None = None):
+    """
+        Updates a given goal, keeps all other goals the same.
+
+        If no day was given, will update for today.
+    """
+    if day is None:
+        day = timezone.now()
+
+    oldLine = getGoal(user, None, day)
+    for statName, value in goal_data.items():
+        setattr(oldLine, statName, value)
+    oldLine.update({
+        'date': day.date(),
+        'user': user
+    })
+
+    Goals.objects.update_or_create(**oldLine)
