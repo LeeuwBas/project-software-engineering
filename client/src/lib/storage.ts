c582ce729fc00c6f8dfde806234682770fc0705c
david kramer <david.kramer2@student.uva.nl>
2026-06-11 11:32:25 +0200

    rewritten storage layout

--- client/src/lib/storage.ts
+++ client/src/lib/storage.ts
@@ -1,16 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const statPrefix = 'Stats-'
+
 interface Settings {
     chosenPet: String,
 }
 
 interface StatLine {
     waterDrank: number,
-}
-
-interface StatisticsData {
-    entries: number,
-    Statistics: Record<string, StatLine>,
 }
 
 export interface StatisticsSummary {
@@ -50,43 +47,56 @@ function calculateDate(date: Date) {
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate() + 1).padStart(2, '0');
 
-    return `${year}-${month}-${day}`
+    return `${statPrefix}${year}-${month}-${day}`
 }
 
 /*
  * Returns the statistic data interface of the given day.
  */
-async function getDayStat(day: Date) {
+async function getStat(day: Date) {
     const date = calculateDate(day);
-    const raw = await AsyncStorage.getItem('Statistics')
+    const raw = await AsyncStorage.getItem(date)
 
-    const data: StatisticsData| null = raw ? JSON.parse(raw) : null;
-
-    if (data == null) {
-        return null
-    }
-
-    return data.Statistics[date];
+    return (raw ? JSON.parse(raw) : null) as StatLine | null;
 }
 
 /*
- * Gets all existing entries of a specific statistic in the given date range.
+ * Gets all existing entries of statistics in the given date range.
+ * Exclusive bound on the lower range and inclusive bound on the upper range (a < b <= c)
  */
-async function getRangeStat(statName: string, lowerDay: Date, upperDay: Date) {
+async function getStatRange(lowerDay: Date, upperDay: Date) {
     const lowerDate = calculateDate(lowerDay);
     const upperDate = calculateDate(upperDay);
 
-    const raw = await AsyncStorage.getItem('Statistics')
-    const data: StatisticsData = raw ? JSON.parse(raw) : null;
+    const dates = (await AsyncStorage.getAllKeys()).filter(
+            (key) => key.startsWith(statPrefix) && lowerDate < key && key <= upperDate
+        ).sort();
 
-    if (data == null) {
+
+    const raw = await AsyncStorage.multiGet(dates);
+    const lines = raw.map(([date, line]): [string, StatLine] => [date, (line ? JSON.parse(line): null)])
+
+    return lines.filter(([_, val]) => val != null)
+}
+
+export async function getNamedStat(statName: string, day: Date) {
+    const line = await getStat(day);
+
+    if (line === null) {
         return null;
     }
 
-    const entries = Object.entries(data.Statistics).filter(([date]) => lowerDate < date && date <= upperDate)
-        .sort(([one], [two]) => (one > two ? -1 : 1));
+    return line[statName as keyof StatLine];
+}
 
-    return Object.values(entries).map(([_, entry]) => entry[statName as keyof StatLine]);
+export async function getNamedStatRange(statName: string, lowerDay: Date, upperDay: Date) {
+    const lines = await getStatRange(lowerDay, upperDay);
+
+    if (lines === null) {
+        return null;
+    }
+
+    return lines.map(([date, line]): [string, number | boolean] => [date, line[statName as keyof StatLine]])
 }
 
 /*
@@ -99,7 +109,7 @@ export async function getStatSummary(statName: string, days: number) {
     const lowerDay = new Date();
     lowerDay.setDate(today.getDate() - days);
 
-    const values = await getRangeStat(statName, lowerDay, today);
+    const values = await getNamedStatRange(statName, lowerDay, today);
 
     if (values == null) {
         return null;
@@ -107,11 +117,14 @@ export async function getStatSummary(statName: string, days: number) {
 
     const returnValue: Partial<StatisticsSummary> = {statisticName: statName};
 
-    returnValue.total = values.reduce((Acc, x, _) => Acc + x);
+    // if (values instanceof [string, number][])
+
+    returnValue.total = values.reduce((Acc, [d, x], _) => Acc + +x, 0);
+    // returnValue.total = values.reduce((Acc, [d, x], _) => Acc + ((x instanceof boolean) ? +x : x), 0);
     returnValue.count = values.length;
     returnValue.average = returnValue.total / returnValue.count;
-    returnValue.maximum = values.reduce((Acc, x, _) => Acc > x ? Acc : x);
-    returnValue.minimum = values.reduce((Acc, x, _) => Acc < x ? Acc : x);
+    returnValue.maximum = values.reduce((Acc, [d, x], _) => Acc > +x ? Acc : +x, 0);
+    returnValue.minimum = values.reduce((Acc, [d, x], _) => Acc < +x ? Acc : +x, 0);
 
     returnValue.isFull = returnValue.count == days
 
@@ -138,7 +151,7 @@ export async function getStatBarChart(statName: string, days: number, binCount: 
     returnValue.isFull = true;
 
     for (let i = 0; i < binCount; i++) {
-        const values = await getRangeStat(statName, lowerDay, today);
+        const values = await getNamedStatRange(statName, lowerDay, today);
 
         if (values == null) {
             return null;
@@ -148,7 +161,7 @@ export async function getStatBarChart(statName: string, days: number, binCount: 
             returnValue.isFull = false;
         }
 
-        returnValue.bins.push(values.reduce((Acc, x, _) => Acc + x) / values.length);
+        returnValue.bins.push(values.reduce((Acc, [d, x], _) => Acc + +x, 0) / values.length);
 
         lowerDay = today
         today.setDate(today.getDate() + returnValue.daysPerBin);
@@ -161,7 +174,7 @@ export async function getStatBarChart(statName: string, days: number, binCount: 
  * Updates a stat, default is to update today, can be changed.
  */
 export async function updateStat(statName: string, change: number, day: Date = new Date()) {
-    var line: StatLine| null = await getDayStat(day);
+    var line: StatLine| null = await getStat(day);
     if (line === null || line === undefined) {
         return null;
     }
@@ -170,18 +183,6 @@ export async function updateStat(statName: string, change: number, day: Date = n
     line[statName as keyof StatLine] = oldVal + change;
 
     AsyncStorage.setItem(calculateDate(day), JSON.stringify(line));
-}
-
-/*
- * Returns the data of a specified statistic at a specified date.
- */
-export async function getStat(statName: string, day: Date = new Date()) {
-    const line = await getDayStat(day);
-    if (line === null || line === undefined) {
-        return null
-    }
-
-    return line[statName as keyof StatLine]
 }
 
 
