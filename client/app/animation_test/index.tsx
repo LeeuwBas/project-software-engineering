b626d8cf5c2988d2f0d7fba240f62a1d2051b2f2
AlexAugustijn <alex.augustijn@student.uva.nl>
2026-06-12 09:13:17 +0200

    Fixed pathing in library

--- client/app/animation_test/index.tsx
+++ client/app/animation_test/index.tsx
@@ -3,7 +3,7 @@ import { Pressable, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { AnimationName } from '@/lib/animations';
+import { AnimationName } from '@/lib/animations/library';
 
 export default function AnimationView() {
     const [currentAnim, setCurrentAnim] = useState<AnimationName>("frog_breath_happy");
@@ -17,7 +17,7 @@ export default function AnimationView() {
                 alignItems: 'center',
             }}
             >
-                <Pressable className="w-4/5 h-3/6 self-center overflow-hidden items-center justify-center" onPress={() => {currentAnim === 'frog_breath_happy' ? setCurrentAnim('frog_drinking') : setCurrentAnim('frog_breath_happy')}}>
+                <Pressable className="w-4/5 h-3/6 self-center overflow-hidden items-center justify-center" onPress={() => {currentAnim === 'frog_breath_neutral' ? setCurrentAnim('frog_sleeping') : setCurrentAnim('frog_breath_neutral')}}>
                     <View  pointerEvents="box-none">
                         <Animation key={currentAnim} animation={currentAnim} scale={9} />
                     </View>
