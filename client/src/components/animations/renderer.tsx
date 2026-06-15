0c1e7c27e15cb70e276ede9c225edf55e7cbf6bc
Dorus-vda <44002080+Dorus-vda@users.noreply.github.com>
2026-06-15 13:34:32 +0200

    fixed animation glitch frame sync problems (hopefully)

--- client/src/components/animations/renderer.tsx
+++ client/src/components/animations/renderer.tsx
@@ -24,21 +24,20 @@ export function Animation({ animation, scale = 1 }: AnimationProps) {
     : ANIMATIONS['placeholder'];
   const image = useImage(config.source);
   const frame = useSharedValue(0);
-  const startTime = useSharedValue<number | null>(null);
+  const startTime = useSharedValue(0);
   
   
   useEffect(() => {
-    startTime.value = null;
+    startTime.value = performance.now();
     frame.value = 0;
   }, [animation]);
 
   // Calculates frame index in asset
   useFrameCallback((info) => {
-    if (startTime.value === null) {
-      startTime.value = info.timeSinceFirstFrame;
-    }
-    const elapsed = info.timeSinceFirstFrame - startTime.value;
+    const elapsed = info.timestamp - startTime.value;
+
     frame.value = Math.floor((elapsed / 1000) * config.fps) % config.frameCount;
+    
   });
 
   // Samples frame based on index and frame size from asset
