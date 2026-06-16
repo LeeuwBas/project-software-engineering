c2bffe61008afbfda3927e1e00dd88ff18a9f664
david kramer <david.kramer2@student.uva.nl>
2026-06-16 13:44:25 +0200

    finished calculatingthe moods

--- client/src/lib/animations/mood.ts
+++ client/src/lib/animations/mood.ts
@@ -1,10 +1,47 @@
-import {StatLine, getCurrentGoal, getStat} from '@/lib/storage'
+import { getCurrentGoal, getStat, StatLine } from '@/lib/storage'
+
+function checkStats(achieved: StatLine, goals: StatLine, fraction: number, hour: number) {
+    (Object.entries(achieved) as [keyof StatLine, number][]).forEach(
+        ([key, value]) => {
+            switch (key) {
+                case "stress":
+                    if (value !== 0) {
+                        return false;
+                    }
+                    break;
+                case "food":
+                    if (hour < 14) {
+                        if (value < 1) {
+                            return false;
+                        }
+                    } else if (hour < 20) {
+                        if (value < 2) {
+                            return false;
+                        }
+                    } else {
+                        if (value < 3) {
+                            return false;
+                        }
+                    }
+                    break;
+                default:
+                    if (value < ((goals[key] ?? 0) * fraction)) {
+                        return false;
+                    }
+            }
+        }
+    );
+}
 
 export async function getMood(): Promise<number> {
     const achieved = await getStat();
     const goals = await getCurrentGoal(null);
 
     if (achieved === null || goals === null) {
+        return 1;
+    }
+
+    if (typeof goals == "number") {
         return 1;
     }
 
@@ -20,9 +57,8 @@ export async function getMood(): Promise<number> {
         // Calculates the fraction of the waking day that has passed.
         // Waking day is defined as being from 10 to 22, so 12 hours.
         const fraction = (hour - 10) / 12;
-        
+        return +checkStats(achieved, goals, fraction, hour) + 1;
     } else {
         return 0;
     }
-    return 0;
 }
