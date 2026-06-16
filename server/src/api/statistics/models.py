ada35701e01c5b358a1b8666b5ce28aa344373dd
david kramer <david.kramer2@student.uva.nl>
2026-06-16 11:28:32 +0200

    prepared server for stress level storage

--- server/src/api/statistics/models.py
+++ server/src/api/statistics/models.py
@@ -13,10 +13,14 @@ class Stats(models.Model):
             date: holds date of the statistic
 
             water: glasses of water drank on the given day
+            stress: stress level
+                    -1: not set
+                    0, 1, 2: low, medium, high
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(default=timezone.now)
     water = models.IntegerField(default=0)
+    stress = models.IntegerField(default=-1)
 
 class Goals(models.Model):
     """
@@ -27,7 +31,11 @@ class Goals(models.Model):
             date: holds start date of the goal
 
             water: glasses of water drank on the given day
+            stress: stress level. Added in goals for parity with Stats.
+                    -1: not set
+                    0, 1, 2: low, medium, high
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(default=timezone.now)
     water = models.IntegerField(default=0)
+    stress = models.IntegerField(default=-1)
