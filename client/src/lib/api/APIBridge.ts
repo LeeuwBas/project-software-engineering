bd63dc45e01cb11237d59a377bc726799a43aec1
david kramer <david.kramer2@student.uva.nl>
2026-06-16 16:46:07 +0200

    oopsie

--- client/src/lib/api/APIBridge.ts
+++ client/src/lib/api/APIBridge.ts
@@ -1,5 +1,6 @@
 import { createWaterBridge } from '@/lib/api/WaterBridge';
 import { loadCalender } from '@/lib/api/GenericStorage';
+import { createStressBridge } from './StressBridge';
 
 export type LoadableBridge<T> = T & Loadable;
 
@@ -56,4 +57,4 @@ export async function getGoalCalender(startDate: Date, endDate: Date) {
 }
 
 export const waterBridge = register(createWaterBridge());
-export const stressBridge = register(createWaterBridge())
+export const stressBridge = register(createStressBridge())
