beab18e54ba617a32eae001be920eff1ab089865
ZJWeng <weng.zj.2005@gmail.com>
2026-06-16 11:20:29 +0200

    replaced icons + small visual changes

--- client/src/components/widgets/Menu.tsx
+++ client/src/components/widgets/Menu.tsx
@@ -1,16 +1,16 @@
+import { AppText } from '@/components/AppText';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import StepsWidget from '@/components/widgets/StepsWidget';
+import WaterWidget from '@/components/widgets/WaterWidget';
 import { useAppContext } from '@/lib/AppContext';
-import { View, TextInput } from 'react-native';
-import { AppText } from '../AppText';
-import StepsWidget from './StepsWidget';
-import WaterWidget from './WaterWidget';
-import { GlassWater, SportShoe } from 'lucide-react-native';
+import Glass from '@assets/icons/module_icons/glass.svg';
+import Shoe from '@assets/icons/module_icons/shoe.svg';
 import { useState } from 'react';
-import { getCurrentGoal } from '@/lib/storage';
+import { TextInput, View } from 'react-native';
 
 export default function Menu({ water, setWater }: { water: number; setWater: Function }) {
-  const [goalsViewActive, setGoalsViewActive] = useState(false)
+  const [goalsViewActive, setGoalsViewActive] = useState(false);
   // TODO: Add backend for retrieving name
   const name = 'Alex';
   const { menuOpen } = useAppContext();
@@ -18,7 +18,8 @@ export default function Menu({ water, setWater }: { water: number; setWater: Fun
     return null;
   }
 
-  const goals = {'water': 10, 'steps': 10000, 'eten': 5}
+  // TODO: Get goals from API
+  const goals = { water: 10, steps: 7000 };
 
   return (
     <View
@@ -27,29 +28,47 @@ export default function Menu({ water, setWater }: { water: number; setWater: Fun
         <Card className="mb-6 h-auto w-3/4 items-center justify-center shadow-block">
           <CardHeader className="w-full flex-1 flex-row items-center justify-between">
             <CardTitle className="mx-2 my-4 text-2xl font-bold">{name}</CardTitle>
-            <Button onPress={() => setGoalsViewActive(!goalsViewActive)} className="py-0">
-              <AppText className="font-bold text-white">Change goals</AppText>
-            </Button>
+            {goalsViewActive ? (
+              <Button
+                onPress={() => setGoalsViewActive(!goalsViewActive)}
+                className="py-0"
+                variant="secondary">
+                <AppText className="font-bold text-white">Confirm</AppText>
+              </Button>
+            ) : (
+              <Button onPress={() => setGoalsViewActive(!goalsViewActive)} className="py-0">
+                <AppText className="font-bold text-white">Change goals</AppText>
+              </Button>
+            )}
           </CardHeader>
-          {/* Normal view */}
-          {!goalsViewActive && (
+          {goalsViewActive ? (
+            // Goals edit view
+            <CardContent className="w-full max-w-full items-start">
+              <AppText className="text-lg font-bold">Goals:</AppText>
+              <View className="flex flex-row items-center gap-2 p-2">
+                <Glass height={30} width={30} />
+                <TextInput
+                  keyboardType="numeric"
+                  inputMode="numeric"
+                  className="rounded-xl border-2 bg-slate-300 px-2"
+                  defaultValue={`${goals['water']}`}
+                />
+              </View>
+              <View className="flex flex-row items-center gap-2 p-2">
+                <Shoe height={30} width={30} />
+                <TextInput
+                  keyboardType="numeric"
+                  inputMode="numeric"
+                  className="rounded-xl border-2 bg-slate-300 px-2"
+                  defaultValue={`${goals['steps']}`}
+                />
+              </View>
+            </CardContent>
+          ) : (
+            // Normal view
             <CardContent className="w-full max-w-full items-center">
               <WaterWidget water={water} setWater={setWater} />
               <StepsWidget />
-            </CardContent>
-          )}
-          {/* Goals view */}
-          {goalsViewActive && (
-            <CardContent className="w-full max-w-full items-start">
-              <AppText className='font-bold text-lg'>Goals:</AppText>
-              <View className="flex flex-row items-center gap-2 p-2">
-                <GlassWater size={30} />
-                <TextInput inputMode='numeric' className='bg-slate-400 border-2 rounded-xl px-2' defaultValue={`${goals['water']}`}></TextInput>
-              </View>
-              <View className="flex flex-row items-center gap-2 p-2">
-                <SportShoe size={30} />
-                <TextInput inputMode='numeric' className='bg-slate-400 border-2 rounded-xl px-2' defaultValue={`${goals['steps']}`}></TextInput>
-              </View>
             </CardContent>
           )}
         </Card>
--- client/src/components/widgets/StepsWidget.tsx
+++ client/src/components/widgets/StepsWidget.tsx
@@ -1,12 +1,18 @@
-import { SportShoe } from 'lucide-react-native';
+import { AppText } from '@/components/AppText';
+import Shoe from '@assets/icons/module_icons/shoe.svg';
 import { View } from 'react-native';
-import { AppText } from '../AppText';
 
 export default function StepsWidget() {
+  const steps = 4298;
+  // TODO: get goal from API
+  const goal = 7000;
+
   return (
     <View className="flex w-full flex-row items-center gap-2 p-2">
-      <SportShoe size={30} />
-      <AppText className="text-base font-bold">9999</AppText>
+      <Shoe height={30} width={30} />
+      <AppText className="text-base font-bold">
+        {steps} / {goal}
+      </AppText>
     </View>
   );
 }
