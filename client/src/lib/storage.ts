cab48eeab5a85e4ab94b4d3c909c3d25f6371b29
david kramer <david.kramer2@student.uva.nl>
2026-06-16 09:11:33 +0200

    changed some goals functions

--- client/src/lib/storage.ts
+++ client/src/lib/storage.ts
@@ -100,8 +100,8 @@ export async function getCurrentGoal(statName: string | null, day: Date = new Da
  * @param statName Name of the goal to change
  * @param goal new value for the goal
  */
-export async function setNewGoal(statName: string, goal: number) {
-    const today = calculateDate(new Date(), false);
+export async function setNewGoal(statName: string, goal: number, date: Date = new Date()) {
+    const today = calculateDate(date, false);
 
     var oldGoal = await getCurrentGoal(null);
     if (oldGoal === null || typeof oldGoal === 'number') {
