6f716469acb0e2723fc0eaa4df8b38294bc145fe
david kramer <david.kramer2@student.uva.nl>
2026-06-16 12:41:48 +0200

    continued work

--- client/src/lib/animations/mood.ts
+++ client/src/lib/animations/mood.ts
@@ -1,8 +1,28 @@
 import {StatLine, getCurrentGoal, getStat} from '@/lib/storage'
 
-export async function getMood() {
+export async function getMood(): Promise<number> {
     const achieved = await getStat();
     const goals = await getCurrentGoal(null);
 
-    Date()
+    if (achieved === null || goals === null) {
+        return 1;
+    }
+
+    const hour = new Date().getHours();
+
+    if (hour < 10) {
+        if (achieved.sleep == 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    } else if (hour < 22) {
+        // Calculates the fraction of the waking day that has passed.
+        // Waking day is defined as being from 10 to 22, so 12 hours.
+        const fraction = (hour - 10) / 12;
+        
+    } else {
+        return 0;
+    }
+    return 0;
 }
