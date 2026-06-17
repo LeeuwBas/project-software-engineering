b854aeb11b0588593dfcafa3ec9c711a5c340a44
WilliamBower <william.mat.bower@gmail.com>
2026-06-17 09:59:54 +0200

    Added sleep and food fields to server

--- server/src/api/statistics/helpers.py
+++ server/src/api/statistics/helpers.py
@@ -8,84 +8,86 @@ from .models import Stats, Goals
 
 def getStatDict(line: Stats | Goals):
     """
-        Pulls all items in stats and goals models into a nice dictionary to
-        read from and update.
+    Pulls all items in stats and goals models into a nice dictionary to
+    read from and update.
 
     """
-    return {
-        'water': line.water
-    }
+    return {"water": line.water, "sleep": line.sleep, "food": line.food}
+
 
 def getSummary(user: str, statistic: str, lowerDay: datetime, upperDay: datetime):
     """
-        Aggregates a summary for the requested statistic, summarizes the past days,
-        amount is given in 'days'.
+    Aggregates a summary for the requested statistic, summarizes the past days,
+    amount is given in 'days'.
     """
 
     filter_dict = {
         "user": user,
-        'date__gt': lowerDay.date(),
-        'date__lte': upperDay.date()
+        "date__gt": lowerDay.date(),
+        "date__lte": upperDay.date(),
     }
 
     day_amount = (upperDay - lowerDay).days
 
     lines = Stats.objects.filter(**filter_dict)
 
-    response:dict = lines.aggregate(total=Sum(statistic),
-                                    low=Min(statistic),
-                                    high=Max(statistic),
-                                    count=Count(statistic))
+    response: dict = lines.aggregate(
+        total=Sum(statistic),
+        low=Min(statistic),
+        high=Max(statistic),
+        count=Count(statistic),
+    )
 
-    response['average'] = response['total']/day_amount
+    response["average"] = response["total"] / day_amount
 
-    if response['count'] < day_amount:
-        response['low'] = 0
-        response['count'] = day_amount
+    if response["count"] < day_amount:
+        response["low"] = 0
+        response["count"] = day_amount
 
     return response
 
-def getBarChart(lowerDate: datetime, upperDate: datetime, bins: int, user: str, statistic: str):
+
+def getBarChart(
+    lowerDate: datetime, upperDate: datetime, bins: int, user: str, statistic: str
+):
     """
-        Returns statistics data aggregated into a format to render a bar chart.
-        days must be a multiple of bins.
+    Returns statistics data aggregated into a format to render a bar chart.
+    days must be a multiple of bins.
     """
-    days_per_bin = (upperDate - lowerDate).days//bins
+    days_per_bin = (upperDate - lowerDate).days // bins
 
     bin_dict = {}
     filter = {
-        'user': user,
-        'date__gt': lowerDate,
-        'date__lte': lowerDate + timedelta(days_per_bin)
+        "user": user,
+        "date__gt": lowerDate,
+        "date__lte": lowerDate + timedelta(days_per_bin),
     }
 
     for i in range(bins):
-        bin_dict[i] = Stats.objects.filter(**filter).aggregate(
-            total=Sum(statistic)
-        )['total']
+        bin_dict[i] = Stats.objects.filter(**filter).aggregate(total=Sum(statistic))[
+            "total"
+        ]
 
         if bin_dict[i] is None:
             bin_dict[i] = 0
 
         bin_dict[i] /= days_per_bin
 
-        filter['date__gt'] = filter['date__lte']
-        filter['date__lte'] += timedelta(days_per_bin)
+        filter["date__gt"] = filter["date__lte"]
+        filter["date__lte"] += timedelta(days_per_bin)
 
     return bin_dict
 
+
 def getDay(user: str, statistic: str | None, day: datetime | None = None):
     """
-        Returns the statistic line of a given day. If a statistic is given it
-        will return just that statistic, if None, will return a dictionary with the full line.
+    Returns the statistic line of a given day. If a statistic is given it
+    will return just that statistic, if None, will return a dictionary with the full line.
     """
     if day is None:
         day = timezone.now()
 
-    filter = {
-        'user': user,
-        'date': day.date()
-    }
+    filter = {"user": user, "date": day.date()}
 
     line = Stats.objects.filter(**filter).first()
 
@@ -97,37 +99,33 @@ def getDay(user: str, statistic: str | None, day: datetime | None = None):
     else:
         return getattr(line, statistic)
 
+
 def setDay(user: str, statistic: dict[str, int], day: datetime | None = None):
     """
-        Sets new data for a specified date.
-        Takes the dictionary in statistics for all data, only overwrites.
+    Sets new data for a specified date.
+    Takes the dictionary in statistics for all data, only overwrites.
     """
     if day is None:
         day = datetime.now()
 
-    filter = {
-        'user': user,
-        'date': day.date()
-    }
+    filter = {"user": user, "date": day.date()}
 
     Stats.objects.update_or_create(**filter, defaults=statistic)
 
+
 def getGoal(user: str, statName: str | None, day: datetime | None = None):
     """
-        Retrieves the latest goal, either for a given stat or just for all stats
-        combined.
+    Retrieves the latest goal, either for a given stat or just for all stats
+    combined.
 
-        If no day was given, will retrieve the most recent goal.
+    If no day was given, will retrieve the most recent goal.
     """
     if day is None:
         day = timezone.now()
 
-    filter = {
-        'user': user,
-        'date__lte': day.date()
-    }
+    filter = {"user": user, "date__lte": day.date()}
 
-    line = Goals.objects.filter(**filter).order_by('-date').first()
+    line = Goals.objects.filter(**filter).order_by("-date").first()
 
     if line is None:
         line = Goals()
@@ -137,11 +135,12 @@ def getGoal(user: str, statName: str | None, day: datetime | None = None):
 
     return getattr(line, statName, None)
 
+
 def setGoal(user: str, goal_data: dict[str, int], day: datetime | None = None):
     """
-        Updates a given goal, keeps all other goals the same.
+    Updates a given goal, keeps all other goals the same.
 
-        If no day was given, will update for today.
+    If no day was given, will update for today.
     """
     if day is None:
         day = timezone.now()
@@ -150,25 +149,22 @@ def setGoal(user: str, goal_data: dict[str, int], day: datetime | None = None):
     for statName, value in goal_data.items():
         oldLine[statName] = value
 
-    filter = {
-        'date': day.date(),
-        'user': user
-    }
+    filter = {"date": day.date(), "user": user}
 
     Goals.objects.update_or_create(**filter, defaults=oldLine)
 
+
 def getCalender(user: str, startDay: datetime, endDay: datetime):
     """
-        Returns a list of dictionaries to render the calendar in the frontend
-        Every item in the dictionaries show if that goal was met that day.
+    Returns a list of dictionaries to render the calendar in the frontend
+    Every item in the dictionaries show if that goal was met that day.
 
-        The dictionaries are in chronological order and are inclusive on both ends.
+    The dictionaries are in chronological order and are inclusive on both ends.
     """
     returnList = []
 
     currentDay = startDay
     while currentDay <= endDay:
-
         stats = getDay(user, None, currentDay)
         goals = getGoal(user, None, currentDay)
 
--- server/src/api/statistics/models.py
+++ server/src/api/statistics/models.py
@@ -3,31 +3,43 @@ from django.utils import timezone
 
 from ..authentication.models import User
 
+
 class Stats(models.Model):
     """
-        Simple stats model.
-        Contains one line per user per day which holds the accumulated stats of
-        the day.
-        data held:
-            user: holds the user id for identification
-            date: holds date of the statistic
+    Simple stats model.
+    Contains one line per user per day which holds the accumulated stats of
+    the day.
+    data held:
+        user: holds the user id for identification
+        date: holds date of the statistic
 
-            water: glasses of water drank on the given day
+        water: glasses of water drank on the given day
+        sleep: good/bad sleep
+        food: amount of meals eaten on the given day
     """
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(default=timezone.now)
     water = models.IntegerField(default=0)
+    sleep = models.IntegerField(default=0)
+    food = models.IntegerField(default=0)
+
 
 class Goals(models.Model):
     """
-        Simple goal model.
-        Contains the goals the user wants to achieve starting on date.
-        data held:
-            user: holds the user id for identification
-            date: holds start date of the goal
+    Simple goal model.
+    Contains the goals the user wants to achieve starting on date.
+    data held:
+        user: holds the user id for identification
+        date: holds start date of the goal
 
-            water: glasses of water drank on the given day
+        water: glasses of water drank on the given day
+        sleep: good/bad sleep
+        food: amount of meals eaten on the given day
     """
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(default=timezone.now)
     water = models.IntegerField(default=0)
+    sleep = models.IntegerField(default=1)
+    food = models.IntegerField(default=3)
