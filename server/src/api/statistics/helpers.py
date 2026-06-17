f237d557b9843da60f3e1dcc5581f1d02ea5bed4
david kramer <david.kramer2@student.uva.nl>
2026-06-17 13:58:41 +0200

    fix as soon as server is updated

--- server/src/api/statistics/helpers.py
+++ server/src/api/statistics/helpers.py
@@ -34,8 +34,8 @@ def getSummary(user: str, statistic: str, lowerDay: datetime, upperDay: datetime
     lines = Stats.objects.filter(**filter_dict)
 
     response:dict = lines.aggregate(total=Sum(statistic),
-                                    low=Min(statistic),
-                                    high=Max(statistic),
+                                    minimum=Min(statistic),
+                                    maximum=Max(statistic),
                                     count=Count(statistic))
 
     response['average'] = response['total']/day_amount
