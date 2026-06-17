0a9980de6a5d1b9ae7690720f4221581997d92bb
david kramer <david.kramer2@student.uva.nl>
2026-06-17 12:53:09 +0200

    documentations

--- client/src/lib/timers.ts
+++ client/src/lib/timers.ts
@@ -1,8 +1,12 @@
 import { initializeApiManager } from "./api/APIBridge";
 import { syncServer } from "./StorageSync";
 
+// Value to check if the day has rolled over
 export let nextTimer = new Date();
 
+/**
+ * Schedules the automatic cache flushes. Called once on startup.
+ */
 export function scheduleCacheFlush() {
     const now = new Date();
     nextTimer = new Date();
@@ -17,6 +21,9 @@ export function scheduleCacheFlush() {
     }, flushTimer)
 }
 
+/**
+ * Callback function for the cache flush timer, reschedules itself.
+ */
 export async function flushCache() {
     await initializeApiManager();
     await syncServer(true);
