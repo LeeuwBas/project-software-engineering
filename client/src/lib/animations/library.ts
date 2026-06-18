3660f92d576bf911f581350c11d9132dc95fc8e1
Dorus-vda <44002080+Dorus-vda@users.noreply.github.com>
2026-06-18 22:44:38 +0200

    iphone animation fix poging 1

--- client/src/lib/animations/library.ts
+++ client/src/lib/animations/library.ts
@@ -109,7 +109,7 @@ export const ANIMATIONS = {
     frog_drinking: {
         width: 64,
         height: 64,
-        frameCount: 60,
+        frameCount: 59,
         fps: 8,
         source: require('@assets/pets/frog/frog_drinking.png'),
     },
