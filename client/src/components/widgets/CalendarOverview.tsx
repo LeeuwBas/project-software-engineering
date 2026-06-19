5b36879cf1295719aaf453465a119e562d78caa6
ZJWeng <weng.zj.2005@gmail.com>
2026-06-19 09:43:42 +0200

    Current day is now black border so show stress of current day

--- client/src/components/widgets/CalendarOverview.tsx
+++ client/src/components/widgets/CalendarOverview.tsx
@@ -109,7 +109,6 @@ export default function CalendarOverview() {
           const data = await getGoalCalender(new Date(rangeStart), new Date(rangeEnd));
           updateCalendarData(data || []);
         }
-
       } catch (error) {
         console.error('Failed to fetch calendar:', error);
       }
@@ -126,8 +125,8 @@ export default function CalendarOverview() {
     setCurrentDate((prev) => new Date(prev.getFullYear(), prev.getMonth() + 1, 1));
   };
 
-  console.log(calendarData[17])
-  
+  console.log(calendarData[today.getDate() - 1]); // Today
+
   return (
     <View>
       {/* Month/year displaty with arrow buttons */}
@@ -170,7 +169,8 @@ export default function CalendarOverview() {
             <View
               className={`m-1 h-16 flex-1 items-center justify-between gap-y-1 border-2
                     ${item.active ? 'border-border' : item.hidden ? 'border-transparent opacity-0' : 'opacity-40'}
-                    ${item.currentDay ? 'bg-secondary' : calendarData[index]?.stress === 2 ? 'bg-[#b41b21]/30' : calendarData[index]?.stress === 1 ? 'bg-[#FFFF00]/30' : calendarData[index]?.stress === 0 ? 'bg-[#22a022]/30' : ''} `}>
+                    ${item.currentDay && 'border-primary-foreground'}
+                    ${calendarData[index]?.stress === 2 ? 'bg-[#b41b21]/30' : calendarData[index]?.stress === 1 ? 'bg-[#FFFF00]/30' : calendarData[index]?.stress === 0 ? 'bg-[#22a022]/30' : ''} `}>
               <AppText className="self-end text-sm font-bold">
                 {item.value || 'Placeholder'}
               </AppText>
