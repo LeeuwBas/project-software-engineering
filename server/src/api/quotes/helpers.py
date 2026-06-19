b1427680a2c3b04ad5f1cd559fe1dea3bcf6fa35
FabiJenne <fabiennevoorhorst@gmail.com>
2026-06-19 14:16:53 +0200

    updated quotes.json structure

--- server/src/api/quotes/helpers.py
+++ server/src/api/quotes/helpers.py
@@ -18,15 +18,15 @@ def load_quotes():
         return json.load(f)
 
 
-def get_json_quote(action, happiness):
+def get_json_quote(action, level, context="Standard"):
     quotes = load_quotes()
     return random.choice(quotes["Neutral"]["WaterLow"]) # temp for testing
 
-    if action not in quotes:
+    if action not in quotes or level not in quotes[action]:
         return "What do you want from me?"
 
-    tier = get_happiness_tier(happiness)
-
-    possible_quotes = quotes[action][tier]
+    tier = quotes[action][level]
+    # Get's the specific context, if it doesn't exist, it defaults to the "Standard" context
+    possible_quotes = tier.get(context, tier.get("Standard", []))
 
     return random.choice(possible_quotes)
