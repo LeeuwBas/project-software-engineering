e29c8ec0092eb5124e914b0a26fe7cd19e1ac58a
david kramer <david.kramer2@student.uva.nl>
2026-06-21 15:19:10 +0200

    created functionality to synchronize the settings page with the server backend

--- client/src/lib/settings.ts
+++ client/src/lib/settings.ts
@@ -1,5 +1,7 @@
 import { create } from 'zustand';
 import { getSettings, setSettings, Settings, createSettings } from '@/lib/storage';
+import { getAPI, postAPI } from './api/ApiManager';
+import { Currency } from 'lucide-react-native';
 
 type SettingsStore = {
     settings: Settings;
@@ -11,23 +13,68 @@ const useSettingsStore = create<SettingsStore>((set) => ({
     setStore: (settings) => set({ settings }),
 }));
 
+/**
+ * Loads the server's settings state. Assumes that it is up to date, so overwrites the local settings page.
+ *
+ * @returns boolean signalling if the operation was a success
+ */
+export async function loadSettingsServer() {
+    const res = await getAPI("/users/settings");
+    if (res === null) {
+        return false;
+    }
+
+    const b64Settings = res["settings"];
+    const settings: Settings = JSON.parse(atob(b64Settings));
+
+    setSettings(settings);
+    return true;
+}
+
+/**
+ * Saves the current locally stored settings to the server.
+ *
+ * @param [settings=null] settings to save to the server, if not provided will store what is on local storage.
+ */
+async function saveSettingsServer(settings: Settings | null = null) {
+    if (settings === null) {
+        settings = await getSettings();
+    }
+
+    const b64Settings = btoa(JSON.stringify(settings));
+    postAPI("/users/settings", {settings: b64Settings});
+}
+
+/**
+ * Stores settings to local storage and to the server database
+ *
+ * @param settings new settings to store
+ */
+async function setSyncSettings(settings: Settings) {
+    saveSettingsServer(settings);
+    await setSettings(settings);
+}
+
 export async function loadSettings() {
     const settings = await getSettings();
     useSettingsStore.getState().setStore(settings);
+    loadSettingsServer().then((success) => console.log(
+        success ? "collected settings from server" : "failed to collect settings from server"
+    ));
 }
 
 async function setTutorialDone() {
     const current = useSettingsStore.getState().settings;
     current.hasDoneTutorial = true;
     useSettingsStore.getState().setStore(current);
-    await setSettings(current);
+    await setSyncSettings(current)
 }
 
 async function resetTutorial() {
     const current = useSettingsStore.getState().settings;
     current.hasDoneTutorial = true;
     useSettingsStore.getState().setStore(current);
-    await setSettings(current);
+    await setSyncSettings(current);
 }
 
 export function useTutorial() {
