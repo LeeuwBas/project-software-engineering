1dadf4acad882c76670e105b8781d3b2bc40e8be
LeeuwBas <bas.leeuw@student.uva.nl>
2026-06-22 15:00:17 +0200

    no log keys

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -3,10 +3,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  AsyncStorage.getAllKeys().then((keys) => {
-    console.log(keys);
-  });
-
   return (
     <RequireNoAuth loading={null}>
       <Redirect href="/onboarding" />
