15080d07a955873b1e1490e78ed9e27f59ab55da
david kramer <david.kramer2@student.uva.nl>
2026-06-22 15:23:49 +0200

    changed pet ID to have frog as ID 0

--- client/src/components/widgets/Pet.tsx
+++ client/src/components/widgets/Pet.tsx
@@ -7,9 +7,9 @@ export default function Pet({ className = '', id, ...props }: { className?: stri
   function selection(id: number) {
     switch (id) {
       case 0:
-        return <Onigiri width={'100%'} height={200} />;
-      case 1:
         return <Frog width={'100%'} height={200} />;
+        case 1:
+        return <Onigiri width={'100%'} height={200} />;
       case 2:
         return <Blob width={'100%'} height={200} />;
       default:
