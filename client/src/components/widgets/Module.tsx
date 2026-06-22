2525b08f954149814eefdfb440aa6dbc458f2957
ZJWeng <weng.zj.2005@gmail.com>
2026-06-22 11:11:17 +0200

    use steps function in types instead of manual call

--- client/src/components/widgets/Module.tsx
+++ client/src/components/widgets/Module.tsx
@@ -1,6 +1,5 @@
 import { AppText } from '@/components/AppText';
 import { Button } from '@/components/ui/button';
-import StepsWidget from '@/components/widgets/StepsWidget';
 import { MenuConfig, ModuleId } from '@/lib/types';
 import ThumbsDown from '@assets/icons/module_icons/thumbs_down.svg';
 import ThumbsUp from '@assets/icons/module_icons/thumbs_up.svg';
@@ -60,8 +59,13 @@ export default function Module({
 
         {id === 'steps' && (
           <View className="mx-auto flex-1 flex-row items-center justify-center">
-            <StepsWidget storedSteps={value} />
-            <AppText className="text-base font-bold"> / {goal}</AppText>
+            <AppText style={{ width: valueWidth * 4 }} className="text-right text-base font-bold">
+              {value}
+            </AppText>
+            <AppText className="w-4 text-center text-base font-bold"> / </AppText>
+            <AppText style={{ width: valueWidth * 4 }} className="text-left text-base font-bold">
+              {goal}
+            </AppText>
           </View>
         )}
 
