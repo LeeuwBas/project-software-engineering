2aed1f9c5dede1163d42f83d9bfb288a191205e2
ZJWeng <weng.zj.2005@gmail.com>
2026-06-23 20:37:41 +0200

    moved hook calls to bar

--- client/src/components/widgets/Bar.tsx
+++ client/src/components/widgets/Bar.tsx
@@ -1,4 +1,4 @@
-import { BarType } from '@/lib/types';
+import { GoaledModule } from '@/lib/types';
 import { View } from 'react-native';
 import { NotchedBox } from '../ui/notched-box';
 
@@ -12,21 +12,24 @@ import { NotchedBox } from '../ui/notched-box';
  *
  * @returns The progress bar
  */
-export default function Bar({ icon: Icon, value, goal, color, borderColor }: BarType) {
+export default function Bar({ module }: { module: GoaledModule }) {
+  const value = module.useValue() ?? 0;
+  const goal = module.bridge.useGoal() ?? 0;
+
   return (
     <View className="w-1/2">
       <View className="flex-row justify-center gap-1">
-        <Icon />
+        <module.icon />
         <NotchedBox
           fillClassName="bg-background"
-          borderStyle={{ backgroundColor: borderColor }}
+          borderStyle={{ backgroundColor: module.borderColor }}
           className="flex-1">
           <View className="absolute inset-1 flex-row overflow-hidden">
             <View
               className="mt-auto h-full"
               style={{
                 width: `${(Math.min(Math.max(value, 0), goal) * 100) / goal}%`,
-                backgroundColor: color,
+                backgroundColor: module.color,
               }}
             />
           </View>
--- client/src/components/widgets/topbar.tsx
+++ client/src/components/widgets/topbar.tsx
@@ -2,7 +2,7 @@ import { AppText } from '@/components/AppText';
 import Bar from '@/components/widgets/Bar';
 import Weather from '@/components/widgets/Weather';
 import { getActiveModules } from '@/lib/settings';
-import { BarType, GoaledModule, MODULES } from '@/lib/types';
+import { GoaledModule, MODULES } from '@/lib/types';
 import { View } from 'react-native';
 
 /**
@@ -17,15 +17,15 @@ export default function Topbar() {
     (module): module is GoaledModule => getActiveModules()[module.id] && 'goalConfig' in module
   );
 
-  // Parameters for each module bar
-  const bars: BarType[] = activeGoaledModules.map((module) => ({
-    id: module.id,
-    icon: module.icon,
-    value: module.useValue() ?? 0,
-    goal: module.bridge.useGoal() ?? 0,
-    color: module.color,
-    borderColor: module.borderColor,
-  }));
+  // // Parameters for each module bar
+  // const bars: BarType[] = activeGoaledModules.map((module) => ({
+  //   id: module.id,
+  //   icon: module.icon,
+  //   value: module.useValue() ?? 0,
+  //   goal: module.bridge.useGoal() ?? 0,
+  //   color: module.color,
+  //   borderColor: module.borderColor,
+  // }));
 
   return (
     <View className="flex-row content-start">
@@ -35,8 +35,8 @@ export default function Topbar() {
       </View>
 
       <View className="w-full flex-col gap-2">
-        {bars.map((bar) => (
-          <Bar {...bar} key={bar.id} />
+        {activeGoaledModules.map((module) => (
+          <Bar module={module} key={module.id} />
         ))}
       </View>
     </View>
