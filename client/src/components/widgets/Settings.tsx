ac4206db6e88d3ae4bc8a45209cba517256cad57
david kramer <david.kramer2@student.uva.nl>
2026-06-23 14:06:09 +0200

    settings menu closes when restarting tutorial

--- client/src/components/widgets/Settings.tsx
+++ client/src/components/widgets/Settings.tsx
@@ -14,7 +14,7 @@ import { SvgProps } from 'react-native-svg';
 /** TODO (buenk): docstring, and some comments throughout, explaining each section */
 export default function Settings() {
   const { resetTutorial } = useTutorial();
-  const { settingsOpen } = useAppContext();
+  const { settingsOpen, changeSettings } = useAppContext();
   const auth = useAuth();
   const router = useRouter();
   const { colorScheme, toggleColorScheme } = useColorScheme();
@@ -49,6 +49,7 @@ export default function Settings() {
     {
       label: 'Restart tutorial',
       effect: () => {
+        changeSettings();
         resetTutorial();
       },
     },
