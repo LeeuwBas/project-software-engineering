375838f8e714a3e3e92107d74bcdf0cb6a9ae7c2
WilliamBower <william.mat.bower@gmail.com>
2026-06-24 11:25:44 +0200

    Move schema docs to separate files for statistics

---
+++ server/src/api/statistics/schemas.py
@@ -0,0 +1,330 @@
+from rest_framework import serializers
+
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiParameter,
+    OpenApiResponse,
+    inline_serializer,
+)
+from drf_spectacular.types import OpenApiTypes
+
+
+STAT_GET_SCHEMA = extend_schema(
+    summary="Retrieves the statistics data of a given date.",
+    description="""Retrieves the statistics of a given date, if no name
+            for the statistic was provided, the API will return all known stats.
+            """,
+    parameters=[
+        OpenApiParameter(
+            name="date",
+            description="date for which the requested goal was active",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.PATH,
+            required=True,
+        ),
+        OpenApiParameter(
+            name="statName",
+            description="Internal name of the goal. If not supplied, "
+            "the api will return all goals at the given date.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=False,
+        ),
+    ],
+    responses={
+        200: {
+            "type": "object",
+            "properties": {
+                "goals": {
+                    "type": "object",
+                    "additionalProperties": {"type": "integer"},
+                    "example": {"water": 5},
+                }
+            },
+        },
+        400: OpenApiResponse(description="Invalid input."),
+    },
+)
+
+STAT_POST_SCHEMA = extend_schema(
+    summary="Updates the given statistics on the given day",
+    description="""Sets or updates the statistics for a day set in the path.
+        Request body must be a dictionary, any key not in the dictionary will not be
+        changed or set to the default value in case there is no data of the day.
+            """,
+    parameters=[
+        OpenApiParameter(
+            name="date",
+            description="Date to insert the data for.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.PATH,
+            required=True,
+        ),
+    ],
+    request={
+        "application/json": {
+            "type": "object",
+            "properties": {
+                "stats": {
+                    "type": "object",
+                    "description": "Statistics to be inserted. <internal_name>:<val>",
+                    "additionalProperties": {"type": "number"},
+                }
+            },
+            "required": ["stats"],
+        }
+    },
+    responses={
+        200: OpenApiResponse(description="Statistics successfully updated or created."),
+        400: OpenApiResponse(description="Invalid input."),
+        401: OpenApiResponse(description="Unauthorised request."),
+    },
+)
+
+STAT_BULK_GET_SCHEMA = extend_schema(
+    summary="Retrieves the set stat at a given date range",
+    description="""Retrieves the stats of a given date range for a given
+                module. If the module is not supplied it will return all stats.
+                """,
+    parameters=[
+        OpenApiParameter(
+            name="start_date",
+            description="Start date for the stats.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=True,
+        ),
+        OpenApiParameter(
+            name="end_date",
+            description="End date of the stats.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=True,
+        ),
+        OpenApiParameter(
+            name="stat_name",
+            description="Internal name of the stat. If not supplied, "
+            "the api will return all stats at the given date.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=False,
+        ),
+    ],
+    responses={
+        200: {
+            "type": "object",
+            "properties": {
+                "date": {
+                    "type": "object",
+                    "additionalProperties": {"type": "integer"},
+                    "example": {"water": 5},
+                }
+            },
+        },
+        401: OpenApiResponse(description="Unauthorised request."),
+    },
+)
+
+STAT_BULK_POST_SCHEMA = extend_schema(
+    summary="Sets a new stat",
+    description="""Updates or inserts a new stat 
+                    for a given date range""",
+    request={
+        "application/json": {
+            "type": "object",
+            "properties": {
+                "date": {
+                    "type": "object",
+                    "description": "Stats that need to be updated. "
+                    "<internal_name>:<val>",
+                    "additionalProperties": {"type": "number"},
+                }
+            },
+        }
+    },
+    responses={
+        200: OpenApiResponse(
+            description="Updated or created stat values for given dates."
+        ),
+        400: OpenApiResponse(description="Invalid Input."),
+        401: OpenApiResponse(description="Unauthorised request."),
+    },
+)
+
+GOAL_GET_SCHEMA = extend_schema(
+    summary="Retrieves the set goal at a given date.",
+    description="""Retrieves the goals of a given dat for a given
+            module. If the module is not supplied it will return all set goals.
+            """,
+    parameters=[
+        OpenApiParameter(
+            name="goal_date",
+            description="Date for which the requested goal was active.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.PATH,
+            required=True,
+        ),
+        OpenApiParameter(
+            name="goal_name",
+            description="Internal name of the goal. If not supplied, "
+            "the api will return all goals at the given date.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=False,
+        ),
+    ],
+    responses={
+        200: {
+            "type": "object",
+            "properties": {
+                "goals": {
+                    "type": "object",
+                    "additionalProperties": {"type": "integer"},
+                    "example": {"water": 5},
+                }
+            },
+        },
+        400: OpenApiResponse(description="Invalid input."),
+        401: OpenApiResponse(description="Unauthorised request."),
+    },
+)
+
+GOAL_POST_SCHEMA = extend_schema(
+    summary="Sets a new goal",
+    description="""Updates or inserts a new goal to be followed for a single date.
+            """,
+    parameters=[
+        OpenApiParameter(
+            name="goal_date",
+            description="Date for which to set the goal.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.PATH,
+            required=True,
+        ),
+    ],
+    request={
+        "application/json": {
+            "type": "object",
+            "properties": {
+                "goals": {
+                    "type": "object",
+                    "description": "Goals that need to be updated."
+                    "<internal_name>:<val>",
+                    "additionalProperties": {"type": "number"},
+                }
+            },
+        }
+    },
+    responses={
+        200: OpenApiResponse(description="Updated goal(s) for specified date."),
+        400: OpenApiResponse(description="Invalid input."),
+        401: OpenApiResponse(description="Unauthorised request."),
+    },
+)
+
+GOAL_BULK_GET_SCHEMA = extend_schema(
+    summary="Retrieves the set goal at a given date range",
+    description="""Retrieves the goals of a given date range for a given
+                module. If the module is not supplied it will return all set goals.
+                """,
+    parameters=[
+        OpenApiParameter(
+            name="start_date",
+            description="Start date for the goals.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=True,
+        ),
+        OpenApiParameter(
+            name="end_date",
+            description="End date of the goal.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=True,
+        ),
+        OpenApiParameter(
+            name="goal_name",
+            description="Internal name of the goal. If not supplied, "
+            "the api will return all goals at the given date.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.QUERY,
+            required=False,
+        ),
+    ],
+    responses={
+        200: {
+            "type": "object",
+            "properties": {
+                "date": {
+                    "type": "object",
+                    "additionalProperties": {"type": "integer"},
+                    "example": {"water": 5},
+                }
+            },
+        },
+        400: OpenApiResponse(description="Invalid input."),
+        401: OpenApiResponse(description="Unauthorised request."),
+    },
+)
+
+GOAL_BULK_POST_SCHEMA = extend_schema(
+    summary="Sets a new goal",
+    description="""Updates or inserts a new goal to be followed. for a given date range
+                """,
+    request={
+        "application/json": {
+            "type": "object",
+            "properties": {
+                "date": {
+                    "type": "object",
+                    "description": "Goals that need to be updated."
+                    "<internal_name>:<val>",
+                    "additionalProperties": {"type": "number"},
+                }
+            },
+        }
+    },
+    responses={
+        200: OpenApiResponse(description="Updated goals for specified dates."),
+        400: OpenApiResponse(description="Invalid input."),
+        401: OpenApiResponse(description="Unauthorised request."),
+    },
+)
+
+CALENDAR_GET_SCHEMA = extend_schema(
+    summary="Retrieves the calendar view of a given date range.",
+    description="""Retrieves boolean data if all goals are completed
+            for a given date range. Inclusive on both sides of the date range
+            (a <= b <= c).
+
+            Element 0 of the return array is the oldest date.
+            """,
+    parameters=[
+        OpenApiParameter(
+            name="start_date",
+            description="Start date of the calendar view.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.PATH,
+            required=True,
+        ),
+        OpenApiParameter(
+            name="end_date",
+            description="End date of the calender view.",
+            type=OpenApiTypes.STR,
+            location=OpenApiParameter.PATH,
+            required=True,
+        ),
+    ],
+    responses={
+        200: inline_serializer(
+            name="MyResponse",
+            fields={
+                "key": serializers.CharField(),
+                "val": serializers.FloatField(),
+            },
+            many=True,
+        ),
+        400: OpenApiResponse(description="Invalid input."),
+        401: OpenApiResponse(description="Unauthorised request."),
+    },
+)
--- server/src/api/statistics/views.py
+++ server/src/api/statistics/views.py
@@ -1,6 +1,5 @@
 from django.forms.models import FieldError
 
-from rest_framework import serializers
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,9 +9,20 @@ from drf_spectacular.utils import (
     extend_schema,
     OpenApiParameter,
     OpenApiResponse,
-    inline_serializer,
 )
 from drf_spectacular.types import OpenApiTypes
+
+from .schemas import (
+    STAT_GET_SCHEMA,
+    STAT_POST_SCHEMA,
+    STAT_BULK_GET_SCHEMA,
+    STAT_BULK_POST_SCHEMA,
+    GOAL_GET_SCHEMA,
+    GOAL_POST_SCHEMA,
+    GOAL_BULK_GET_SCHEMA,
+    GOAL_BULK_POST_SCHEMA,
+    CALENDAR_GET_SCHEMA,
+)
 
 from datetime import datetime, timedelta
 
@@ -90,43 +100,7 @@ class StatManageView(APIView):
     permission_classes = [IsAuthenticated, IsSelf]
     serializer_class = StatsSerializer
 
-    @extend_schema(
-        summary="Retrieves the statistics data of a given date.",
-        description="""Retrieves the statistics of a given date.
-            If no name for the statistic was provided,
-            the API will return all known stats.
-            """,
-        parameters=[
-            OpenApiParameter(
-                name="date",
-                description="Date for which the requested goal was active.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.PATH,
-                required=True,
-            ),
-            OpenApiParameter(
-                name="statName",
-                description="Internal name of the stat. If not supplied, "
-                "the api will return all stats at the given date.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.QUERY,
-                required=False,
-            ),
-        ],
-        responses={
-            200: {
-                "type": "object",
-                "properties": {
-                    "goals": {
-                        "type": "object",
-                        "additionalProperties": {"type": "integer"},
-                        "example": {"water": 5},
-                    }
-                },
-            },
-            401: OpenApiResponse(description="Unauthorised request."),
-        },
-    )
+    @STAT_GET_SCHEMA
     def get(self, request, date):
         statName = request.query_params.get("statName", None)
         day = datetime.fromisoformat(date)
@@ -141,43 +115,7 @@ class StatManageView(APIView):
 
         return Response(returnVal, HTTP_200_OK)
 
-    @extend_schema(
-        summary="Updates the given statistics on the given day",
-        description="""Sets or updates the statistics for a day set in the path.
-        Request body must be a dictionary, any key not in the dictionary will not be
-        changed or set to the default value in case there is no data of the day.
-            """,
-        parameters=[
-            OpenApiParameter(
-                name="date",
-                description="Date to insert the data for.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.PATH,
-                required=True,
-            ),
-        ],
-        request={
-            "application/json": {
-                "type": "object",
-                "properties": {
-                    "stats": {
-                        "type": "object",
-                        "description": "Statistics to be inserted. "
-                        "<internal_name>:<val>",
-                        "additionalProperties": {"type": "number"},
-                    }
-                },
-                "required": ["stats"],
-            }
-        },
-        responses={
-            200: OpenApiResponse(
-                description="Statistics successfully updated or created."
-            ),
-            400: OpenApiResponse(description="Invalid input."),
-            401: OpenApiResponse(description="Unauthorised request."),
-        },
-    )
+    @STAT_POST_SCHEMA
     def post(self, request, date):
         stat_data = request.data.get("stats")
         day = datetime.fromisoformat(date)
@@ -191,49 +129,7 @@ class StatBulkView(APIView):
     permission_classes = [IsAuthenticated, IsSelf]
     serializer_class = StatsSerializer
 
-    @extend_schema(
-        summary="Retrieves the set stat at a given date range",
-        description="""Retrieves the stats of a given date range for a given
-                module. If the module is not supplied it will return all stats.
-                """,
-        parameters=[
-            OpenApiParameter(
-                name="start_date",
-                description="Start date for the stats.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.QUERY,
-                required=True,
-            ),
-            OpenApiParameter(
-                name="end_date",
-                description="End date of the stats.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.QUERY,
-                required=True,
-            ),
-            OpenApiParameter(
-                name="stat_name",
-                description="Internal name of the stat. If not supplied, "
-                "the api will return all stats at the given date.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.QUERY,
-                required=False,
-            ),
-        ],
-        responses={
-            200: {
-                "type": "object",
-                "properties": {
-                    "date": {
-                        "type": "object",
-                        "additionalProperties": {"type": "integer"},
-                        "example": {"water": 5},
-                    }
-                },
-            },
-            401: OpenApiResponse(description="Unauthorised request."),
-        },
-    )
+    @STAT_BULK_GET_SCHEMA
     def get(self, request):
         stat_name = request.query_params.get("stat_name", None)
         start_date = request.query_params.get("start_date", None)
@@ -263,31 +159,7 @@ class StatBulkView(APIView):
 
         return Response(return_dict, HTTP_200_OK)
 
-    @extend_schema(
-        summary="Sets a new stat",
-        description="""Updates or inserts a new stat 
-                    for a given date range""",
-        request={
-            "application/json": {
-                "type": "object",
-                "properties": {
-                    "date": {
-                        "type": "object",
-                        "description": "Stats that need to be updated. "
-                        "<internal_name>:<val>",
-                        "additionalProperties": {"type": "number"},
-                    }
-                },
-            }
-        },
-        responses={
-            200: OpenApiResponse(
-                description="Updated or created stat values for given dates."
-            ),
-            400: OpenApiResponse(description="Invalid Input."),
-            401: OpenApiResponse(description="Unauthorised request."),
-        },
-    )
+    @STAT_BULK_POST_SCHEMA
     def post(self, request):
         for date in request.data:
             data = request.data[date]
@@ -444,43 +316,7 @@ class GoalManageView(APIView):
     permission_classes = [IsAuthenticated, IsSelf]
     serializer_class = StatsSerializer
 
-    @extend_schema(
-        summary="Retrieves the set goal at a given date.",
-        description="""Retrieves the goals of a given dat for a given
-            module. If the module is not supplied it will return all set goals.
-            """,
-        parameters=[
-            OpenApiParameter(
-                name="goal_date",
-                description="Date for which the requested goal was active.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.PATH,
-                required=True,
-            ),
-            OpenApiParameter(
-                name="goal_name",
-                description="Internal name of the goal. If not supplied, "
-                "the api will return all goals at the given date.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.QUERY,
-                required=False,
-            ),
-        ],
-        responses={
-            200: {
-                "type": "object",
-                "properties": {
-                    "goals": {
-                        "type": "object",
-                        "additionalProperties": {"type": "integer"},
-                        "example": {"water": 5},
-                    }
-                },
-            },
-            400: OpenApiResponse(description="Invalid input."),
-            401: OpenApiResponse(description="Unauthorised request."),
-        },
-    )
+    @GOAL_GET_SCHEMA
     def get(self, request, goal_date):
         goal_name = request.query_params.get("goal_name", None)
         if goal_name is None:
@@ -497,38 +333,7 @@ class GoalManageView(APIView):
         else:
             return Response({goal_name: goals}, HTTP_200_OK)
 
-    @extend_schema(
-        summary="Sets a new goal",
-        description="""Updates or inserts a new goal to be followed for a single date.
-            """,
-        parameters=[
-            OpenApiParameter(
-                name="goal_date",
-                description="Date for which to set the goal.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.PATH,
-                required=True,
-            ),
-        ],
-        request={
-            "application/json": {
-                "type": "object",
-                "properties": {
-                    "goals": {
-                        "type": "object",
-                        "description": "Goals that need to be updated."
-                        "<internal_name>:<val>",
-                        "additionalProperties": {"type": "number"},
-                    }
-                },
-            }
-        },
-        responses={
-            200: OpenApiResponse(description="Updated goal(s) for specified date."),
-            400: OpenApiResponse(description="Invalid input."),
-            401: OpenApiResponse(description="Unauthorised request."),
-        },
-    )
+    @GOAL_POST_SCHEMA
     def post(self, request, goal_date):
         goal_data = request.data.get("goals")
         day = datetime.fromisoformat(goal_date)
@@ -547,50 +352,7 @@ class GoalBulkView(APIView):
     permission_classes = [IsAuthenticated, IsSelf]
     serializer_class = StatsSerializer
 
-    @extend_schema(
-        summary="Retrieves the set goal at a given date range",
-        description="""Retrieves the goals of a given date range for a given
-                module. If the module is not supplied it will return all set goals.
-                """,
-        parameters=[
-            OpenApiParameter(
-                name="start_date",
-                description="Start date for the goals.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.QUERY,
-                required=True,
-            ),
-            OpenApiParameter(
-                name="end_date",
-                description="End date of the goal.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.QUERY,
-                required=True,
-            ),
-            OpenApiParameter(
-                name="goal_name",
-                description="Internal name of the goal. If not supplied, "
-                "the api will return all goals at the given date.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.QUERY,
-                required=False,
-            ),
-        ],
-        responses={
-            200: {
-                "type": "object",
-                "properties": {
-                    "date": {
-                        "type": "object",
-                        "additionalProperties": {"type": "integer"},
-                        "example": {"water": 5},
-                    }
-                },
-            },
-            400: OpenApiResponse(description="Invalid input."),
-            401: OpenApiResponse(description="Unauthorised request."),
-        },
-    )
+    @GOAL_BULK_GET_SCHEMA
     def get(self, request):
         goal_name = request.query_params.get("goal_name", None)
         start_date = request.query_params.get("start_date", None)
@@ -620,29 +382,7 @@ class GoalBulkView(APIView):
 
         return Response(return_dict, HTTP_200_OK)
 
-    @extend_schema(
-        summary="Sets a new goal",
-        description="""Updates or inserts a new goal to be followed. for a given date range
-                """,
-        request={
-            "application/json": {
-                "type": "object",
-                "properties": {
-                    "date": {
-                        "type": "object",
-                        "description": "Goals that need to be updated."
-                        "<internal_name>:<val>",
-                        "additionalProperties": {"type": "number"},
-                    }
-                },
-            }
-        },
-        responses={
-            200: OpenApiResponse(description="Updated goals for specified dates."),
-            400: OpenApiResponse(description="Invalid input."),
-            401: OpenApiResponse(description="Unauthorised request."),
-        },
-    )
+    @GOAL_BULK_POST_SCHEMA
     def post(self, request):
         for date in request.data:
             data = request.data[date]
@@ -665,43 +405,7 @@ class CalendarView(APIView):
     permission_classes = [IsAuthenticated, IsSelf]
     serializer_class = StatsSerializer
 
-    @extend_schema(
-        summary="Retrieves the calendar view of a given date range.",
-        description="""Retrieves boolean data if all goals are completed
-            for a given date range. Inclusive on both sides of the date range
-            (a <= b <= c).
-
-            Element 0 of the return array is the oldest date.
-            """,
-        parameters=[
-            OpenApiParameter(
-                name="start_date",
-                description="Start date of the calendar view.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.PATH,
-                required=True,
-            ),
-            OpenApiParameter(
-                name="end_date",
-                description="End date of the calender view.",
-                type=OpenApiTypes.STR,
-                location=OpenApiParameter.PATH,
-                required=True,
-            ),
-        ],
-        responses={
-            200: inline_serializer(
-                name="MyResponse",
-                fields={
-                    "key": serializers.CharField(),
-                    "val": serializers.FloatField(),
-                },
-                many=True,
-            ),
-            400: OpenApiResponse(description="Invalid input."),
-            401: OpenApiResponse(description="Unauthorised request."),
-        },
-    )
+    @CALENDAR_GET_SCHEMA
     def get(self, request, start_date, end_date):
         startDay = datetime.fromisoformat(start_date)
         endDay = datetime.fromisoformat(end_date)
